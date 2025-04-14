d611e90a5cef07054e15c932a6921723bd0fc9dd
MonteBurns1 <144336632+MonteBurns1@users.noreply.github.com>
2025-04-15 01:45:18 +0200

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,28 +1,9 @@
 export default function createIteratorObject(report) {
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  const iterator = {
-    next() {
-      if (currentDepartmentIndex >= report.departments.length) {
-        return { done: true };
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-
-      const currentDepartment = report.departments[currentDepartmentIndex];
-      const currentEmployee = currentDepartment.employees[currentEmployeeIndex];
-
-      currentEmployeeIndex++;
-      if (currentEmployeeIndex >= currentDepartment.employees.length) {
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
-      }
-
-      return { value: currentEmployee, done: false };
-    },
-    [Symbol.iterator]() {
-return this;
-    },
-  };
-
-  return iterator;
-}      
+    }
+  }());
+}
